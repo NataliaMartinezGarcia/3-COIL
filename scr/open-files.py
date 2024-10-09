4f8a6617087dcf99d09950693a15e8191b3b2ac9
luciaveganavarrete <lucia.vega.navarrete@udc.es>
2024-10-09 10:37:15 +0200

    Añadir función open_file que lee un archivo general (dependiendo de la extensión de este, llama a la función que le corresponda de las anteriores)

--- scr/open-files.py
+++ scr/open-files.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import sqlite3
 from sqlalchemy import create_engine
+import os
 
 def open_csv(file_path):
     return pd.read_csv(file_path) # Devuelve un dataframe
@@ -11,12 +12,28 @@ def open_excel(file_path):
 def open_sql(file_path):
     conn = sqlite3.connect(file_path) # Crea una conexión con la base de datos
     cur = conn.cursor() # Crea un cursor para ejecutar sentencias SQL
+    # Devuelve una tupla con los nombres de las tablas que haya
     res = cur.execute("SELECT name FROM sqlite_master")
-    table_name = res.fetchone()[0] # Consigue el nombre de la tabla (asumiendo que solo hay una)
+    # Consigue el nombre de la tabla (asumiendo que solo hay una)
+    table_name = res.fetchone()[0]
     conn.close() # Termina la conexión
 
     # Una vez tenemos el nombre de la tabla, la convertimos a un DataFrame
     engine = create_engine('sqlite:///' + file_path) # Crea un objeto engine
     df = pd.read_sql_table(table_name, engine) # Carga la tabla de la base de datos a un DataFrame
-    engine.dispose()    
+    engine.dispose()  
     return df
+
+def open_file(file_path):
+    # Se asume que el archivo existe!
+    EXTENSIONS = ('.csv', '.xlsx', '.xls', '.db', '.sqlite') # Posibles extensiones
+    # Dependiendo de la extensión le asocia una función
+    EXTENSION_MAP = {'.csv': open_csv, '.xlsx': open_excel, '.xls': open_excel,
+                     '.db': open_sql, '.sqlite': open_sql}
+    
+    _, extension = os.path.splitext(file_path) # Extrae la extensión (incluye el punto)
+
+    # Comprueba que nos pasan archivo válido
+    assert extension in EXTENSIONS, "Formato de archivo inválido"
+    # Extrae el dataframe con la función que corresponda
+    return EXTENSION_MAP[extension](file_path)
